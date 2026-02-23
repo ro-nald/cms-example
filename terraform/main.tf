b1bfe162d8a55c093fb0bfa587a4711bbbd889f2
ro-nald <daddsronald@gmail.com>
2026-02-23 18:43:43 +0000

    Update Terraform module input values

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 module "webapp_example" {
   source = "git::https://github.com/ro-nald/terraform-modules.git//modules/aws/ec2_single-az-with-back-up"
 
-  environment_name  = "cms-example"
-  web_instance_type = "t3.micro"
+  application_name  = "cms-example"
+  region = "ap-east-1"
+  environment = "dev"
 }
